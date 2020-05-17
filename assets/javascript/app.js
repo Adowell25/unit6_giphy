b682f4545b5f8c18223bb2fdd2e91932aa737102
Adowell25 <Adowell25@gmail.com>
2020-05-17 16:34:59 -0500

    submit button adds one button at a time

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -14,3 +14,59 @@ function createButtons(){
 }
 
 createButtons();
+
+$("#animalTxt").on('click', function(event){
+    event.preventDefault();
+    $("#animalButtons").empty();
+    var animalData  = $("#data-input").val().trim();
+    starterAnimals.push(animalData);
+    createButtons();
+
+});
+
+
+//Gif functions
+$("#animalTxt").on('click', function(event){
+    event.preventDefault();
+    var btnData = $(this).attr("animalName");
+    var animalValue = btnData;
+    
+    var queryUrl = "https://api.giphy.com/v1/gifs/search?q=" + animalValue
+                    + "&api_key=J087uKXq8yTPVSnBh2tL8fFOIuWvN4qf&limit=10";
+    $.ajax({
+        url: queryUrl,
+        method: "GET"
+    }).then(function(response) {
+        console.log(response);
+
+        var imageUrl = response.data.images.downsized.url;
+
+        var imgDiv = $("<img>");
+            imgDiv.attr("src", imageUrl);
+            imgDiv.attr("alt", "animal image");
+
+        $("#resultsContainer").append(imgDiv);
+
+
+        // for ( var i = 0; i < response.length; i++){
+        //     var gifRating = response[i].rating;
+        //     var stillGif = response[i].images.downsized_still.url;
+        //     var liveGif = response[i].images.downsized_medium.url;
+
+        //     var gifDiv = $("<div>").addClass("gifDiv");
+        //     var imgDiv = $("<div>").addClass("imgDiv");
+        //     var rating = $("<p>Rating: "+ gifRating + "</p>");
+        //     var gifImg = $("<img alt='img'>").attr("src", stillGif);
+        //         gifImg.attr("data-src", liveGif);
+
+        //     $("reslutsContainer").append(gifDiv);
+        //     gifDiv.append(imgDiv);
+        //     imgDiv.append(rating).append(gifImg);
+
+
+        // }
+    });
+
+});
+
+
